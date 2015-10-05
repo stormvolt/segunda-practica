0ffd8406d17fcb40568fe97fe1042b5c6593853b
stormvolt <luisnu97@hotmail.com>
2015-10-05 17:30:21 -0500

    Update e5.cpp

--- e5.cpp
+++ e5.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
+
+int main() {
+   char cadena[128];
+   ofstream fs("nombre.txt"); 
+   fs << "Hola, mundo" << endl;
+   fs.close();
+   ifstream fe("nombre.txt"); 
+   fe.getline(cadena, 128);
+   cout << cadena << endl;
+   return 0;
+}
